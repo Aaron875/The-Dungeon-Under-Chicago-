d1aa3948e3cf5b8da8ed2e8c0ba9a8611904f6d0
lpd8987 <lpd8987@rit.edu>
2021-12-05 17:08:55 -0500

    Should be ready to build

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public enum buttonType
 {
     Start,
+    Lore,
     Quit,
     Retry,
 }
@@ -16,6 +17,11 @@ public class ButtonBehavior : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if(type == buttonType.Lore)
+        {
+            SceneManager.LoadScene("GameLore");
+        }
+
         if(type == buttonType.Start || type == buttonType.Retry)
         {
             SceneManager.LoadScene("Room1");
